122c5b3fb183c1eaf860994c0422f3e9f9d3817a
Priyanshu Sharma <sharmapriyanshu7164196@gmail.com>
2025-05-30 18:35:29 +0530

    Fix Loan Calculator js code

--- Scholastikit/Loan Calculator/scripts.js
+++ Scholastikit/Loan Calculator/scripts.js
@@ -31,14 +31,10 @@ function getValues() {
  * and then displays each months updated amortization schedule on the page
  */
 function amort(balance, interestRate, terms) {
-  //Calculate the per month interest rate
-  var monthlyRate = interestRate / 12;
+  var simpleInterest = balance * interestRate * (terms / 12); // rate is already in decimal
+  var totalPayment = balance + simpleInterest;
+  var monthlyPayment = totalPayment / terms;
 
-  //Calculate the payment
-  var payment =
-    balance * (monthlyRate / (1 - Math.pow(1 + monthlyRate, -terms)));
-
-  //begin building the return string for the display of the amort table
   var result =
     "Loan amount: " +
     balance.toFixed(2) +
@@ -51,57 +47,33 @@ function amort(balance, interestRate, terms) {
     "<br />" +
     "<br />" +
     "Monthly Payment: " +
-    payment.toFixed(2) +
+    monthlyPayment.toFixed(2) +
     "<br />" +
     "Total Payment: " +
-    (payment * terms).toFixed(2) +
+    totalPayment.toFixed(2) +
     "<br /><br />";
 
-  //add header row for table to return string
   result +=
-    "<table border='1'><tr><th>Month #</th><th>Balance</th>" +
-    "<th>Interest</th><th>Principal</th>";
+    "<table border='1'><tr><th>Month #</th><th>Monthly Payment</th><th>Interest</th><th>Principal</th>";
 
-  /**
-   * Loop that calculates the monthly Loan amortization amounts then adds
-   * them to the return string
-   */
-  for (var count = 0; count < terms; ++count) {
-    //in-loop interest amount holder
-    var interest = 0;
+  var monthlyInterest = simpleInterest / terms;
+  var monthlyPrincipal = balance / terms;
 
-    //in-loop monthly principal amount holder
-    var monthlyPrincipal = 0;
-
-    //start a new table row on each loop iteration
+  for (var i = 0; i < terms; ++i) {
     result += "<tr align=center>";
-
-    //display the month number in col 1 using the loop count variable
-    result += "<td data-column= Month>" + (count + 1) + "</td>";
-
-    //code for displaying in loop balance
-    result += "<td data-column= Balance> " + balance.toFixed(2) + "</td>";
-
-    //calc the in-loop interest amount and display
-    interest = balance * monthlyRate;
-    result += "<td data-column= Interest> " + interest.toFixed(2) + "</td>";
-
-    //calc the in-loop monthly principal and display
-    monthlyPrincipal = payment - interest;
+    result += "<td data-column=Month>" + (i + 1) + "</td>";
     result +=
-      "<td data-column= Principal> " + monthlyPrincipal.toFixed(2) + "</td>";
-
-    //end the table row on each iteration of the loop
+      "<td data-column='Monthly Payment'>" +
+      monthlyPayment.toFixed(2) +
+      "</td>";
+    result +=
+      "<td data-column='Interest'>" + monthlyInterest.toFixed(2) + "</td>";
+    result +=
+      "<td data-column='Principal'>" + monthlyPrincipal.toFixed(2) + "</td>";
     result += "</tr>";
-
-    //update the balance for each loop iteration
-    balance = balance - monthlyPrincipal;
   }
 
-  //Final piece added to return string before returning it - closes the table
   result += "</table>";
-
-  //returns the concatenated string to the page
   return result;
 }
 
